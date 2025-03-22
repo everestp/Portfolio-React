4efdce241431529865b9c237a37e0c8b26499b63
Everest Paudel <paudeleverest09@gmail.com>
2025-03-22 18:16:06 +0545

    initialize React project with Vite, Tailwind CSS, and basic components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+     <h1 className='text-red-900 font-bold'>This isn the  navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
